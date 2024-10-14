2f53c4373ed1ac3e66e61e1e4f67b63a4e1fd7b0
jedo5ooo <jsg998877@gmail.com>
2024-10-14 21:12:54 +0900

    feat_timecapsule 공개글 조회, 삭제 구현

--- src/main/java/com/jnu/festival/domain/timeCapsule/controller/TimeCapsuleController.java
+++ src/main/java/com/jnu/festival/domain/timeCapsule/controller/TimeCapsuleController.java
@@ -1,6 +1,8 @@
 package com.jnu.festival.domain.timeCapsule.controller;
 
 import com.jnu.festival.domain.timeCapsule.dto.TimeCapsuleRequestDto;
+import com.jnu.festival.domain.timeCapsule.dto.TimecapsuleListResponseDto;
+import com.jnu.festival.domain.timeCapsule.dto.TimecapsuleResponseDto;
 import com.jnu.festival.domain.timeCapsule.service.TimeCapsuleService;
 import com.jnu.festival.global.security.UserDetailsImpl;
 import com.jnu.festival.global.util.ResponseDto;
@@ -11,6 +13,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -24,6 +27,23 @@ public class TimeCapsuleController {
         timeCapsuleService.createTimeCapsule(request, images, userDetails);
 
         return ResponseEntity.ok().body(ResponseDto.created(null));
+    }
+
+    @DeleteMapping(value = "/{timecapsuleId}")
+    public ResponseEntity<?> deleteTimeCapsule(@PathVariable Long timecapsuleId, @AuthenticationPrincipal UserDetailsImpl userDetails) throws Exception {
+        timeCapsuleService.deleteTimeCapsule(timecapsuleId, userDetails);
+
+        return ResponseEntity.ok().body(ResponseDto.created(null));
+    }
+
+    @GetMapping(value = "")
+    public ResponseEntity<?> getTimecapsules(@AuthenticationPrincipal UserDetailsImpl userDetails) throws Exception{
+
+        List<TimecapsuleResponseDto> my_timecapsules = timeCapsuleService.getMytimecapsules(userDetails);
+        List<TimecapsuleResponseDto> timecapsules = timeCapsuleService.getTimecapsules(userDetails);
+        TimecapsuleListResponseDto response = timeCapsuleService.mapResponses(my_timecapsules, timecapsules);
+
+        return ResponseEntity.ok().body(ResponseDto.ok(response));
     }
 
 
--- src/main/java/com/jnu/festival/domain/timeCapsule/service/TimeCapsuleService.java
+++ src/main/java/com/jnu/festival/domain/timeCapsule/service/TimeCapsuleService.java
@@ -1,6 +1,8 @@
 package com.jnu.festival.domain.timeCapsule.service;
 
 import com.jnu.festival.domain.timeCapsule.dto.TimeCapsuleRequestDto;
+import com.jnu.festival.domain.timeCapsule.dto.TimecapsuleListResponseDto;
+import com.jnu.festival.domain.timeCapsule.dto.TimecapsuleResponseDto;
 import com.jnu.festival.domain.timeCapsule.entity.TimeCapsule;
 import com.jnu.festival.domain.timeCapsule.entity.TimeCapsuleImage;
 import com.jnu.festival.domain.timeCapsule.repository.TimeCapsuleImageRepository;
@@ -17,7 +19,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -53,6 +57,82 @@ public class TimeCapsuleService {
             // TimeCapsuleImage를 저장
             timeCapsuleImageRepository.save(timeCapsuleImage);
         }
+    }
+
+    @Transactional
+    public void deleteTimeCapsule(Long timecapsuleId, UserDetailsImpl userDetails) throws IOException{
+        // 지우려는 사람의 정보를 가져옴.
+        User user = userRepository.findByNickname(userDetails.getUsername())
+                .orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND_USER));
+
+
+        // 관련된 사진을 S3에서 삭제
+        // 1. 해당 타임캡슐을 DB에서 찾음.
+        TimeCapsule timecapsule = timeCapsuleRepository.findById(timecapsuleId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND_TIMECAPSULE));
+
+        // 타임캡슐 생성자와 현재 사용자가 동일한지 확인
+        if (!timecapsule.getUser().equals(user)) {
+            throw new BusinessException(ErrorCode.NOT_MATCH_USER);
+        }
+
+        // 2. 관련된 사진들을 타임캡슐로 찾는다.
+        List<TimeCapsuleImage> timecapsuleImages = timeCapsuleImageRepository.findTimeCapsuleImagesByTimeCapsule(timecapsule);
+
+        // 3. TimeCapsuleImage 엔티티들에서 URL 추출
+        List<String> urlList = timecapsuleImages.stream()
+                .map(TimeCapsuleImage::getUrl)
+                .collect(Collectors.toList());
+
+        for (String url : urlList){
+            s3Service.delete(url);
+        }
+        timeCapsuleImageRepository.deleteAll(timecapsuleImages);
+        timeCapsuleRepository.delete(timecapsule);
+    }
+
+    public List<TimecapsuleResponseDto> getMytimecapsules(UserDetailsImpl userDetails){
+        User user = userRepository.findByNickname(userDetails.getUsername())
+                .orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND_USER));
+
+        List<TimeCapsule> myTimecapsules = timeCapsuleRepository.findByUser(user);
+
+        return myTimecapsules.stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    public List<TimecapsuleResponseDto> getTimecapsules(UserDetailsImpl userDetails){
+        User user = userRepository.findByNickname(userDetails.getUsername())
+                .orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND_USER));
+        List<TimeCapsule> timecapsules = timeCapsuleRepository.findByIsPublicTrueAndIdIsNot(user);
+
+        return timecapsules.stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    private TimecapsuleResponseDto convertToDto(TimeCapsule timecapsule){
+        List<TimeCapsuleImage> timeCapsuleImages = timeCapsuleImageRepository.findTimeCapsuleImagesByTimeCapsule(timecapsule);
+        List<String> imageUrls = timeCapsuleImages.stream()
+                .map(TimeCapsuleImage::getUrl)
+                .collect(Collectors.toList());
+
+        return TimecapsuleResponseDto.builder()
+                .id(timecapsule.getId())
+                .nickname(timecapsule.getUser().getNickname())
+                .content(timecapsule.getContent())
+                .imageUrls(imageUrls)
+                .created_at(timecapsule.getCreatedAt().toLocalDate())
+                .build();
+    }
+
+    public TimecapsuleListResponseDto mapResponses(List<TimecapsuleResponseDto> my_timecapsules,List<TimecapsuleResponseDto> timecapsules){
+        TimecapsuleListResponseDto response = TimecapsuleListResponseDto.builder()
+                .my_timecapsules(my_timecapsules)
+                .timecapsules(timecapsules)
+                .build();
+        return response;
     }
 
 }
