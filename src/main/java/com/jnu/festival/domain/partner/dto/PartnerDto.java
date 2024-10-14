0774ef421f31a215ba317941b464e8b6c6bc700e
cosmos-1885 <dmsrkql104321@gmail.com>
2024-10-14 19:24:51 +0900

    feat: Partner API 구현

--- src/main/java/com/jnu/festival/domain/partner/dto/PartnerDto.java
+++ src/main/java/com/jnu/festival/domain/partner/dto/PartnerDto.java
@@ -1,13 +1,34 @@
-package com.jnu.festival.domain.partner.DTO;
+package com.jnu.festival.domain.partner.dto;
+
+import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
-public record PartnerDto(
-        Long id,
-        String name,
-        String location,
-        LocalDate startDate,
-        LocalDate endDate,
-        String description
-) {
+@Getter
+public class PartnerDto {
+    private Long id;
+    private String name;
+    private String description;
+    private List<String> images;
+    private String location;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Boolean bookmark;
+    private LocalDateTime createdAt;
+
+    @Builder
+    public PartnerDto(Long id, String name, String description, List<String> images, String location, LocalDate startDate, LocalDate endDate, Boolean bookmark, LocalDateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.images = images;
+        this.location = location;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.bookmark = bookmark;
+        this.createdAt = createdAt;
+    }
 }
