022d920f908dd215ca0e49add35f55e3da4d3d81
진미나 <mina200238@gmail.com>
2024-11-01 00:33:31 +0900

    feat: Add validation  for restricted string length to createBooth,createContents,createZone,createFeedback,createContent,createTimecapsule

--- src/main/java/com/jnu/festival/domain/timecapsule/dto/request/TimecapsuleRequestDto.java
+++ src/main/java/com/jnu/festival/domain/timecapsule/dto/request/TimecapsuleRequestDto.java
@@ -1,7 +1,10 @@
 package com.jnu.festival.domain.timecapsule.dto.request;
 
+import jakarta.validation.constraints.Size;
+
 public record TimecapsuleRequestDto(
         String mailAddress,
+        @Size(max = 500, message = "Timecapsule Content는 500자를 초과할 수 없습니다.")  // 맥시멈 500
         String content,
         Boolean isPublic
 ) {
